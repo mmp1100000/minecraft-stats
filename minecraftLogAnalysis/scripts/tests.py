9112bd13a1689aa09c33c15597b911a4c45621e9
Marco Muñoz Pérez <mmunoz@cash-converters.es>
2020-03-08 11:12:31 +0100

    Added some tests

--- minecraftLogAnalysis/scripts/tests.py
+++ minecraftLogAnalysis/scripts/tests.py
@@ -6,8 +6,12 @@ conn = FtpConnection()
 
 dates = conn.ftp_get_dir_files('logs/')
 
-dates_mod = ["-".join(date.split("-", 3)[:3]) for date in dates]
+# dates_mod = ["-".join(date.split("-", 3)[:3]) for date in dates]
 
 # dates_mod = map(methodcaller("split", "-", 3), dates)
 
-print(sorted(set(dates_mod)))
+print(sorted(set(dates)))
+
+dates_sorted = sorted(set(dates))
+
+print([date for date in dates_sorted if '2020-02-19' in date])
