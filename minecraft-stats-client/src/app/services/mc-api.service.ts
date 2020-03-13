e853f4f7412ef0f16e19a6a7151b29ff4add2d6f
Marco Muñoz Pérez <mmunoz@cash-converters.es>
2020-03-13 18:35:41 +0100

    Removed unused milliseconds representation

--- minecraft-stats-client/src/app/services/mc-api.service.ts
+++ minecraft-stats-client/src/app/services/mc-api.service.ts
@@ -39,7 +39,7 @@ export class McApiService {
     minutes = (minutes < 10) ? '0' + minutes : minutes;
     seconds = (seconds < 10) ? '0' + seconds : seconds;
 
-    return hours + ':' + minutes + ':' + seconds + '.' + milliseconds;
+    return hours + ':' + minutes + ':' + seconds;
   }
 
 }
